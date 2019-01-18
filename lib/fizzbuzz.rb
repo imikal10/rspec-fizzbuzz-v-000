17ed155813c9a058aa68e908bacfe8490b0f1f7c
Isaiah Magett <imagett10@gmail.com>
2019-01-18 23:28:10 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -10,12 +10,6 @@ end
 
 
 
-# my code
-# def fizzbuzz(user_input)
-  user_input = gets.strip
-fizzbuzz(user_input/3) ="Fizz"
-fizzbuzz(user_input/5) ="Buzz"
-fizzbuzz(user_input/15) ="Fizzbuzz"
-# end
+
 
 
